c0a6fd0c171397491461c20b60b12a582937095a
kobiyoyo <adamachubi@gmail.com>
2021-01-14 17:35:54 +0100

    fixed jump error

--- app/serializers/transaction_serializer.rb
+++ app/serializers/transaction_serializer.rb
@@ -2,5 +2,5 @@ class TransactionSerializer < ActiveModel::Serializer
   attributes :id, :transaction_type, :description, :amount, :status, :confirm
   has_one :user
   has_one :wallet
-  has_one :transaction
+  has_one :currency
 end
